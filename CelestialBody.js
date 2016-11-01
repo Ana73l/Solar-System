c4c56c5866269e97931ccd70d18834f0a524dfbf
Ana73l <b0b094abv@gmail.com>
2016-11-01 12:26:40 +0200

    Add files via upload
    added more planets, scale 1-1000, planet scale 1:10

---
+++ CelestialBody.js
@@ -0,0 +1,71 @@
+class CelestialBody {
+  constructor(radius,texture,positionX,orbitsAround,rotSpeed,orbitSpeed) {
+    //the actual body, referred as body.children[0]
+    this.self = new THREE.Mesh(
+      new THREE.SphereGeometry(radius,32,32),
+      new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load(texture)
+      })
+    );
+    this.self.position.set(positionX,0,200);
+
+    //its pivot for orbitting around another body
+    this.body = new THREE.Object3D();
+    this.body.position.set(orbitsAround.children[0].position.x,
+      orbitsAround.children[0].position.y,
+       orbitsAround.children[0].position.z);
+    this.body.rotSpeed = rotSpeed;
+    this.body.orbitSpeed = orbitSpeed;
+
+    //attach camera to this, reffered as body.chidren[1]
+    this.forCam = new THREE.Object3D();
+    this.forCam.radius = radius;
+    this.forCam.position.set(positionX,0,200);
+
+    this.body.add(this.self);
+    this.body.add(this.forCam);
+    orbitsAround.add(this.body);
+    spaceBodies.push(this.body);
+    return this.body;
+  }
+}
+
+function  moveBodies(bodies) {
+  _(bodies).forEach(function(body){
+    body.children[0].rotation.y += body.rotSpeed;
+    body.rotation.y += body.orbitSpeed;
+
+  });
+}
+
+function addCamToBody(lookAt,attBody,camera) {
+  lookAt.children[1].rotation.y += attBody.orbitSpeed;
+  lookAt.children[1].add(camera);
+  camera.position.set(attBody.children[1].position.x,
+    attBody.children[1].position.y,
+      attBody.children[1].position.z);
+
+}
+
+/*this.radius = radius;
+this.texture = texture;
+this.self = new THREE.Mesh(
+  new THREE.SphereGeometry(this.radius,32,32),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(this.texture)
+  })
+);
+this.position = orbitsAround;
+this.pivot = new THREE.Object3D();
+this.pivot.position.set(orbitsAround.children[0].position.x,
+  orbitsAround.children[0].position.y,
+   orbitsAround.children[0].position.z);
+this.self.position.set(positionX,0,200);
+this.self.rotSpeed = rotSpeed;
+this.pivot.orbitSpeed = orbitSpeed;
+this.pivot.add(this.self);
+this.pivot1 = new THREE.Object3D();
+this.pivot1.position.set(positionX+10,0,200);
+this.pivot.add(this.pivot1);
+orbitsAround.add(this.pivot);
+spaceBodies.push(this.pivot);*/
--- src.js
+++ src.js
@@ -1,18 +1,16 @@
-var scene = new THREE.Scene(),
- camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000000),
- renderer = new THREE.WebGLRenderer({ antialias: true }),
+ var scene = new THREE.Scene(),
+ solCenter = new THREE.Object3D(),
+ camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000000000000),
+ renderer = new THREE.WebGLRenderer({ antialias: true}),
  controls = new THREE.OrbitControls(camera, renderer.domElement),
- earth,
- moon,
- star,
- mercuryPivot,
- earthPivot,
- moonPivot;
+ spaceBodies = [];
 
 
 function init() {
   //scale 1 000 000 = 1 000
+
   camera.position.z = 500000;
+
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
@@ -24,72 +22,58 @@ function init() {
   var pointLight = new THREE.PointLight(0xFFFFF);
   pointLight.position.z = 100;
 
+  scene.add(solCenter);
   //sun
-  var starGeometry = new THREE.SphereGeometry(695.7, 32, 32);
-  var starTexture = new THREE.TextureLoader().load('sun.jpg');
-  var starMaterial = new THREE.MeshBasicMaterial({ map: starTexture });
-  star = new THREE.Mesh(starGeometry, starMaterial);
+  var star = new CelestialBody(695.7,'sun.jpg',0,scene,0.01,0);
 
   //mercury
-  mercuryPivot = new THREE.Object3D();
-  mercuryPivot.position = star.position;
-  star.add(mercuryPivot);
-  var mercuryGeometry = new THREE.SphereGeometry(2.44, 32, 32);
-  var mercuryTexture = new THREE.TextureLoader().load('mercury.jpg');
-  var mercuryMaterial = new THREE.MeshBasicMaterial({ map: mercuryTexture });
-  mercury = new THREE.Mesh(mercuryGeometry, mercuryMaterial);
-  mercury.position.set(58000,100,200);
-  mercuryPivot.add(mercury);
+  var mercury = new CelestialBody(200.44,'mercury.jpg',57910,star,0.02,0.002);
+
+  //venus
+  var venus = new CelestialBody(600.05,'venus.jpg',108200,star,0.02,0.0015);
 
   //earth
-  earthPivot = new THREE.Object3D();
-  earthPivot.position = star.position;
-  star.add(earthPivot);
-  var earthGeometry = new THREE.SphereGeometry(6.371, 32, 32);
-  var earthTexture = new THREE.TextureLoader().load('earth.jpg');
-  var earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
-  earth = new THREE.Mesh(earthGeometry, earthMaterial);
-  earth.position.set(150000,100,200);
-  earthPivot.add(earth);
+  var earth = new CelestialBody(600.371,'earth.jpg',149600,star,0.1,0.005);
+  //var rgeometry = new THREE.RingGeometry( 149900, 150100, 40 );
+  //  var rmaterial = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+  //var rmesh = new THREE.Mesh( rgeometry, rmaterial );
+  //scene.add( rmesh );
 
   //moon
-  moonPivot = new THREE.Object3D();
-  moonPivot.position = earth.position;
-  earth.add(moonPivot);
-  var moonGeometry = new THREE.SphereGeometry(1.737, 32, 32);
-  var moonTexture = new THREE.TextureLoader().load('moon.jpg');
-  var moonMaterial = new THREE.MeshBasicMaterial( { map: moonTexture });
-  moon = new THREE.Mesh(moonGeometry, moonMaterial);
-  moon.position.set(3840, 100, 200);
-  moonPivot.add(moon);
+  var moon = new CelestialBody(100.737,'moon.jpg',384,earth,0.001,0.01);
 
+  //mars
+  var mars = new CelestialBody(300.389,'mars.jpg',228000,star,0.01,0.01);
+
+  //jupiter
+  var jupiter = new CelestialBody(690.911,'jupiter.png',778500,star,0.01,0.01);
 
   //particles
   var geometry = new THREE.Geometry();
 	for ( var i = 0; i < 10000; i ++ ) {
 		var vertex = new THREE.Vector3();
-		vertex.x = THREE.Math.randFloatSpread( 10000000 );
-		vertex.y = THREE.Math.randFloatSpread( 10000000 );
-		vertex.z = THREE.Math.randFloatSpread( 10000000 );
+		vertex.x = THREE.Math.randFloatSpread( 100000000000 );
+		vertex.y = THREE.Math.randFloatSpread( 100000000000 );
+		vertex.z = THREE.Math.randFloatSpread( 100000000000 );
 		geometry.vertices.push( vertex );
 	}
 	var particles = new THREE.Points( geometry, new THREE.PointsMaterial( { color: 0x888888 } ) );
 
+  scene.add(camera);
   scene.add(pointLight);
 	scene.add(particles);
-  scene.add(star);
+  window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 }
 
 function render() {
   requestAnimationFrame(render);
 
-  earth.rotation.y -= 0.02;
-  //earth.position.x += -2;
-  moon.rotation.y += 0.1;
-  moonPivot.rotation.y += 0.001;
-  earthPivot.rotation.y += 0.01;
-  mercuryPivot.rotation.y += 0.01;
-  //moon.position.x += -2;
-
+  //addCamToBody(spaceBodies[0],spaceBodies[1],camera);
+  moveBodies(spaceBodies);
   renderer.render(scene, camera);
 }
